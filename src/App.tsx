2212a77e793137204d26efcea85eaccf14147cb6
Patryk Gajewski <patryk.gajewski@arteia.xyz>
2020-06-13 17:52:07 +0200

    Add base routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router, Link, Route, Switch,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <li>
+          <Link to="/register"> Registration</Link>
+        </li>
+        <li>
+          <Link to="/login"> Login </Link>
+        </li>
+        <li>
+          <Link to="/account"> Account </Link>
+        </li>
+      </div>
+
+      <Switch>
+        <Route path="/register" render={() => <div> Registration </div>} />
+        <Route path="/login" render={() => <div> Login </div>} />
+        <Route path="/account" render={() => <div> Account </div>} />
+        <Route path="/" render={() => <div> Home</div>} />
+      </Switch>
+
+    </Router>
   );
 }
 
