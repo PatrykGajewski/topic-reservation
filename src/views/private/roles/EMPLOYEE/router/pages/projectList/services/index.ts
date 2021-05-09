c9453895466ec631ea979dc1e80ef15dd3a6b2d8
patryk.gajewski <patryk.gajewski@arteia.xyz>
2021-05-09 23:26:57 +0200

    Resolve create promoter topic issue

--- src/views/private/roles/EMPLOYEE/router/pages/projectList/services/index.ts
+++ src/views/private/roles/EMPLOYEE/router/pages/projectList/services/index.ts
@@ -2,48 +2,6 @@ import {APISecured} from '../../../../../../../../API';
 import {Project, ProjectDegree} from '../../../../../../../../models/project';
 import { ProjectStatus, ProjectType } from "../../../../../../../../models/project/models";
 
-
-export const _reserveProject = async (projectId: string): Promise<Project> => {
-  try {
-    const { data, error } = await APISecured.patch(`/projects/reserve/${projectId}`, {});
-    if (error) {
-      console.error(error);
-      return Promise.reject();
-    }
-    return Promise.resolve(data.data);
-  } catch (error) {
-    console.error(error);
-    return Promise.reject();
-  }
-};
-
-interface GetAvailableProjectsParams {
-  projectType: ProjectType | null,
-  searchString: string | null,
-  skip: number,
-  limit: number,
-}
-
-export const _fetchAvailableProjects = async (params: GetAvailableProjectsParams): Promise<Project[]> => {
-  try {
-    const { data, error } = await APISecured.post('/projects', {
-      type: params.projectType,
-      searchString: params.searchString,
-      status: ProjectStatus.AVAILABLE,
-      skip: params.skip,
-      limit: params.limit,
-    });
-    if (error) {
-      console.log(error);
-      return Promise.reject(new Error('Cannot get userProjects'));
-    }
-    return Promise.resolve(data.data as Project[]);
-  } catch (err) {
-    console.log(err);
-    return Promise.reject(new Error('API error - userProjects route'));
-  }
-};
-
 interface CreateProjectParams {
   topic: string,
   description: string,
@@ -80,7 +38,7 @@ export const _createProject = async (params: CreateProjectParams): Promise<Proje
       console.log(error);
       return Promise.reject();
     }
-    return Promise.resolve(data.data as Project);
+    return Promise.resolve(data.entry as Project);
   } catch (e) {
     console.log(e);
     return Promise.reject();
